a1235a055f3788e22f508dabca293f6af2754fb8
bhagyashri <bhagyashri@e-arth.in>
2021-05-01 16:27:40 +0530

    handle response

--- src/_helpers/handle-response.js
+++ src/_helpers/handle-response.js
@@ -1,4 +1,3 @@
-import { authenticationService } from '@/_services';
 import { history } from '@/_helpers';
 
 export function handleResponse(response) {
@@ -7,13 +6,6 @@ export function handleResponse(response) {
         if (!response.ok) {
             if ([401, 403].indexOf(response.status) !== -1) {
                 console.log("401,403")
-                // auto logout if 401 Unauthorized or 403 Forbidden response returned from api
-
-                
-                // alert("going to logout")
-                // authenticationService.logout();
-                // history.push('/login');
-                // location.reload(true);
             }
             else if ([400].indexOf(response.status) !== -1) {
                 return data;
@@ -23,33 +15,5 @@ export function handleResponse(response) {
             return Promise.reject(error);
         }
         return data;
-    });
-}
-
-export function handleLoginResponse(response) {
-    return response.text().then(text => {
-        const data = text && JSON.parse(text);
-        if (!response.ok) {
-            const error = (data && {"message" : data.message,"status": data.status}) || {"message" : response.statusText,"status": 500};
-            return Promise.reject(error);
-        }
-        return data;
-    });
-}
-
-export function handleFileResponse(response) {
-    return response.text().then(text => {
-        if(response.status !== 200){
-            const data = text && JSON.parse(text);
-            const error = (data && {"message" : data.message,"status": data.status});
-                return Promise.reject(error);
-        }
-        else{
-            if (!response.ok) {
-                const error = (data && {"message" : response.statusText,"status": response.status});
-                return Promise.reject(error);
-            }
-        }
-        return text;
     });
 }
